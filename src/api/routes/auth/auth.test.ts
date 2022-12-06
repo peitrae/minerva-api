66fa09a498883540411a10aa11aaff33a9073226
Peitra Erdi <peitrae@gmail.com>
2022-12-06 23:21:10 +0700

    Fix AuthService is not define in unit test

--- src/api/routes/auth/auth.test.ts
+++ src/api/routes/auth/auth.test.ts
@@ -2,11 +2,12 @@ import request from 'supertest';
 import { AuthService } from '@/services';
 import { generateRandomString } from '@/utils';
 import { expressApp, server } from '@/app';
+import Container from 'typedi';
 
 jest.mock('@/services/auth/Auth');
 
 describe('routes/auth', () => {
-	const mockAuthService = AuthService as jest.MockedClass<typeof AuthService>;
+	const mockAuthService = AuthService as any;
 
 	afterAll((done) => {
 		server.close();
@@ -20,27 +21,17 @@ describe('routes/auth', () => {
 				refreshToken: 'mockRefreshToken',
 				expiresIn: 3600,
 			};
-			const mockAuthServiceProperties = jest.requireActual(
-				'@/services/auth/Auth'
-			);
+			mockAuthService.login = jest.fn().mockResolvedValue(mockLoginResult);
+			Container.set(AuthService, mockAuthService);
 
-			mockAuthService.mockReturnValue({
-				...mockAuthServiceProperties,
-				login: jest.fn().mockResolvedValue(mockLoginResult),
-			});
+			const res = await request(expressApp)
+				.post(`/api/v1/auth/login`)
+				.send({
+					code: generateRandomString(20),
+				})
+				.expect(200);
 
-			// const res = await request(expressApp)
-			// 	.post(`/api/v1/auth/login`)
-			// 	.send({
-			// 		code: generateRandomString(20),
-			// 	})
-			// 	.expect(200);
-
-			// expect(res.body).toEqual(mockLoginResult);
-
-			/**
-			 * @TODO This test is still error due to AuthService Container is undefined in jest environment.
-			 */
+			expect(res.body).toEqual(mockLoginResult);
 		});
 
 		it('should "Authorization code is not exist" message and status of 400 if code is not exist', async () => {
